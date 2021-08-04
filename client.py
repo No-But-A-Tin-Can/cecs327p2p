56ec4439aac620907abf4ff6f392cfe13c45a10b
Matt H <matthewhoang.hoang@gmail.com>
2021-08-04 13:57:44 -0700

    changed port

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import sys
 # Creation of a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPv4 SOCK_STREAM = TCP
 
-sock.connect((socket.gethostname(), 10000))
+sock.connect((socket.gethostname(), 9999))
 
 msg = sock.recv(1024)
 print(msg.decode("utf-8"))

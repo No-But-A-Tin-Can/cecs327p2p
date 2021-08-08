e40053edda77eb72674a6ead0df0c73ed238f297
Matt H <matthewhoang.hoang@gmail.com>
2021-08-08 12:01:29 -0700

    Added variable for list of files in directory

--- client.py
+++ client.py
@@ -1,13 +1,23 @@
 import socket
 import sys
+import time
 
+#Get the host and port to be used
 HOST = socket.gethostbyname(socket.gethostname())
+
+#Using a port that is not in use
 PORT = 9999
+
+list_of_files = []
+
 
 # AF_INET = IPv4 SOCK_STREAM = TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
-    sock.sendall(b'Hello, world!')
-    data = sock.recv(1024)
+    while True:
+        time.sleep(2)
+        sock.sendall(b'Test message')
+        data = sock.recv(1024)
+        print('Recieved', repr(data))
             
-print('Recieved', repr(data))
+

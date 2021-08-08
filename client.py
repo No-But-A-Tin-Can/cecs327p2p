0d456f02bca8db668089fa6ccce46a887913e972
Matt H <matthewhoang.hoang@gmail.com>
2021-08-07 18:52:21 -0700

    Updated client and server

--- client.py
+++ client.py
@@ -1,17 +1,17 @@
 import socket
 import sys
 
-# Creation of a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET = IPv4 SOCK_STREAM = TCP
+class Client:
 
-sock.connect((socket.gethostbyname(socket.gethostname()), 9999))
+    def __init__(self):
+        HOST = socket.gethostbyname(socket.gethostname())
+        PORT = 9999
 
-full_msg = ''
-while True:
-    msg = sock.recv(1024)
-    print(len(msg))
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
-
-print(full_msg)
+        # AF_INET = IPv4 SOCK_STREAM = TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((HOST, PORT))
+            s.sendall(b'Hello, world!')
+            data = s.recv(1024)
+            full_msg = ''
+            
+        print('Recieved', repr(data))
--- server.py
+++ server.py
@@ -1,22 +1,27 @@
 import socket
 import sys
 
-# Creation of a TCP/IP socket
+class Server:
 
-#Get the host and port to be used
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 9999
+    def __init__(self):
+        #Get the host and port to be used
+        HOST = socket.gethostbyname(socket.gethostname())
+        PORT = 9999
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock: # AF_INET = IPv4 SOCK_STREAM = TCP
-    server_address = (HOST, PORT) # server address and port number
-    print("Starting up on %s port %s" % server_address)
-    sock.bind(server_address)
-    sock.listen() # begins waiting for connections
-    clientsocket, address = sock.accept()
-    with clientsocket:
-        print(f"Connection from {address} has been established.")
-        while True:
-            data = clientsocket.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+        # AF_INET = IPv4 SOCK_STREAM = TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # server address and port number
+            server_address = (HOST, PORT) 
+            print("Starting up on %s port %s" % server_address)
+            #bind to a port
+            sock.bind(server_address)
+            # begins waiting for connections
+            sock.listen() 
+            clientsocket, address = sock.accept()
+            with clientsocket:
+                print(f"Connection from {address} has been established.")
+                while True:
+                    data = clientsocket.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)

25fb840b37200884f2013fabc989a4f2476ad136
Matt H <matthewhoang.hoang@gmail.com>
2021-08-08 20:12:35 -0700

    Added simple methods

--- client.py
+++ client.py
@@ -3,22 +3,35 @@ import sys
 import time
 import os
 
-#Get the host and port to be used
+#turns a list into a string
+def list_to_string(list1):
+    stringed_list = '/'.join(list1)
+    return stringed_list
 
+#turns a string into a list
+def string_to_list(string1):
+    listed_string = string1.split('/')
+    return listed_string
+
+#-----------------------------------------
+
+#Get the host and port to be used
 HOST = socket.gethostbyname(socket.gethostname())
 #Using a port that is not in use
 PORT = 9999
 
-#list of files in the directory at the start of the 
-list_of_files = [file.name for file in os.scandir() if file.is_file()]
-print(list_of_files)
+encoded_list = '/'.join(list_of_files).encode('utf-8')
+#[item.encode('utf-8') for item in list_of_files]
+print(encoded_list)
 
 # AF_INET = IPv4 SOCK_STREAM = TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    #list of files in folder 
+    list_of_files = [file.name for file in os.scandir() if file.is_file() and file.name.endswith('.txt')]
     sock.connect((HOST, PORT))
     while True:
         time.sleep(2)
-        sock.sendall(b'Test message')
+        sock.sendall(encoded_list)
         data = sock.recv(1024)
         print('Recieved', repr(data))
             
--- server.py
+++ server.py
@@ -1,29 +1,57 @@
 import socket
 import sys
+import time
+import os
 
+#compares two lists and returns a list of files the server needs and a list of files the client needs
+def compare_files(server_list, client_list):
+    server_set = set(server_list)
+    client_set = set(client_list)
+
+    client_needs = server_set - client_set
+    server_needs = client_set - server_set
+    print(type(client_needs))
+    return [server_needs, client_needs]
+
+#turns a list into a string
+def list_to_string(list1):
+    stringed_list = '/'.join(list1)
+    return stringed_list
+
+#turns a string into a list
+def string_to_list(string1):
+    listed_string = string1.split('/')
+    return listed_string
+
+#------------------------------------------
 
 #Get the host and port to be used
 HOST = socket.gethostbyname(socket.gethostname())
 #Using a port that is not in use
 PORT = 9999
 
-list_of_files = []
-
 # AF_INET = IPv4 SOCK_STREAM = TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    
+    #list of files in folder
+    list_of_files = [file.name for file in os.scandir() if file.is_file() and file.name.endswith('.txt')]
     # server address and port number
     server_address = (HOST, PORT) 
     print("Starting up on %s port %s" % server_address)
     #bind to a port
     sock.bind(server_address)
-    # begins waiting for connections
-    sock.listen() 
-    clientsocket, address = sock.accept()
-    with clientsocket:
-        print(f"Connection from {address} has been established.")
-        while True:
-            data = clientsocket.recv(1024)
-            print(data)
-            if not data:
-                break
-            clientsocket.sendall(data)
+    while True:
+        # begins waiting for connections
+        sock.listen() 
+        clientsocket, address = sock.accept()
+        with clientsocket:
+            print(f"Connection from {address} has been established.")
+            while True:
+                #Recieve files that the client has
+                data = clientsocket.recv(1024)
+                data = data.decode()
+                client_file_list = data.split('/')
+                needed_files_list = compare_files(list_of_files, client_file_list)
+                if not data:
+                    break
+                clientsocket.sendall(data.encode('utf-8'))

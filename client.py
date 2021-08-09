aabe953581a057615179827381970d0d271ef293
Matt H <matthewhoang.hoang@gmail.com>
2021-08-08 18:50:10 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -2,23 +2,25 @@ import socket
 import sys
 import time
 import os
+import pickle
 
 #Get the host and port to be used
-
 HOST = socket.gethostbyname(socket.gethostname())
 #Using a port that is not in use
 PORT = 9999
 
 #list of files in the directory at the start of the 
-list_of_files = [file.name for file in os.scandir() if file.is_file()]
-print(list_of_files)
+list_of_files = [file.name for file in os.scandir() if file.is_file() and file.name.endswith('.txt')]
+encoded_list = '/'.join(list_of_files).encode('utf-8')
+#[item.encode('utf-8') for item in list_of_files]
+print(encoded_list)
 
 # AF_INET = IPv4 SOCK_STREAM = TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
     while True:
         time.sleep(2)
-        sock.sendall(b'Test message')
+        sock.sendall(encoded_list)
         data = sock.recv(1024)
         print('Recieved', repr(data))
             
--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 import socket
 import sys
-
+import time
+import os
+import pickle
 
 #Get the host and port to be used
 HOST = socket.gethostbyname(socket.gethostname())
 #Using a port that is not in use
 PORT = 9999
 
-list_of_files = []
+list_of_files = [file.name for file in os.scandir() if file.is_file() and file.name.endswith('.txt')]
 
 # AF_INET = IPv4 SOCK_STREAM = TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -16,14 +18,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     print("Starting up on %s port %s" % server_address)
     #bind to a port
     sock.bind(server_address)
-    # begins waiting for connections
-    sock.listen() 
-    clientsocket, address = sock.accept()
-    with clientsocket:
-        print(f"Connection from {address} has been established.")
-        while True:
-            data = clientsocket.recv(1024)
-            print(data)
-            if not data:
-                break
-            clientsocket.sendall(data)
+    while True:
+        # begins waiting for connections
+        sock.listen() 
+        clientsocket, address = sock.accept()
+        with clientsocket:
+            print(f"Connection from {address} has been established.")
+            while True:
+                data = clientsocket.recv(1024)
+                data = data.decode()
+                client_file_list = data.split('/')
+                print(client_file_list)
+                print(data)
+                if not data:
+                    break
+                clientsocket.sendall(data.encode('utf-8'))
